1fae45a514688dde0f575219ed1b03bdb8774f22
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2017-11-11 22:32:02 +0100

    fetch all elements with only one upload; stores the data in local file

--- src/main/tagdb.go
+++ src/main/tagdb.go
@@ -4,7 +4,10 @@ import (
 	"log"
 	"os"
 	vision "cloud.google.com/go/vision/apiv1"
+	pb "google.golang.org/genproto/googleapis/cloud/vision/v1"
 	"golang.org/x/net/context"
+	"github.com/golang/protobuf/proto"
+	"io/ioutil"
 )
 
 func main() {
@@ -14,7 +17,7 @@ func main() {
 		log.Fatalf("Failed to create client: %v", err)
 	}
 
-	filename := "/home/isa/Photos/originaux/2010/2010-05-29/IMGP8086.JPG"
+	filename := "/home/isa/Photos/voyages/Californie/yosemite/p1070504.jpg"
 
 	file, err := os.Open(filename)
 	if err != nil {
@@ -27,14 +30,27 @@ func main() {
 		log.Fatalf("Failed to create image: %v", err)
 	}
 
-	labels, err := client.DetectLabels(ctx, image, nil, 10)
+	res, err := client.AnnotateImage(ctx, &pb.AnnotateImageRequest {
+		Image: image,
+		Features: []*pb.Feature{
+			{Type: pb.Feature_LANDMARK_DETECTION, MaxResults: 5},
+			{Type: pb.Feature_LABEL_DETECTION, MaxResults: 10},
+			{Type: pb.Feature_IMAGE_PROPERTIES},
+		},
+	})
 
 	if err != nil {
 		log.Fatalf("Failed to detect labels: %v", err)
 	}
 
-	fmt.Println("Labels:")
-	for _, label := range labels {
-		fmt.Println(label)
+
+	data, err := proto.Marshal(res)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
 	}
+	ioutil.WriteFile("/home/isa/tmp/protobuf", data, 0644)
+
+
+	fmt.Println(res)
+
 }
